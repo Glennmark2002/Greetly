63fe9ea19e71ae693598ae331b4e435920e7cbda
Glennmark2002 <glennmark.culibra@gmail.com>
2025-04-11 18:18:35 +0800

    create google auth api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoutes from './routes/auth.route.js';
 
 const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(3000);
+mongoose.connect(process.env.MONGO);
+
+app.get('/', (req, res) => res.json({ message : 'API is Working'}));
+app.use('/api/auth', authRoutes);
