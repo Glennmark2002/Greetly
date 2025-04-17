735af1ee64e6a79a1a49f61eab095faeca818e25
Glennmark2002 <glennmark.culibra@gmail.com>
2025-04-17 20:40:46 +0800

    Test Placeholder Date

--- client/src/pages/RequestForm.jsx
+++ client/src/pages/RequestForm.jsx
@@ -27,9 +27,9 @@ function RequestForm() {
   return (
     <div className='h-screen flex justify-center items-center'>
       <form onSubmit={handleSubmit} className='max-w-lg p-4 flex flex-col items-center gap-4 w-full'>
-        <textarea onChange={handleChange} id='purpose' required placeholder='purpose' maxLength={199} className='bg-base-300 w-11/12 p-3 rounded-2xl text-center max-w-sm h-40 resize-none' />
+        <textarea onChange={handleChange} id='purpose' required placeholder='Purpose' maxLength={199} className='bg-base-300 w-11/12 p-3 rounded-2xl text-center max-w-sm h-40 resize-none' />
         <input    onChange={handleChange} id='contactNumber' required type='number' placeholder='Contact Number' className='bg-base-300 w-11/12 p-3 rounded-2xl text-center max-w-sm'></input>
-        <input    onChange={handleChange} id='appointmentDate' type='datetime-local' required className='bg-base-300 w-11/12 p-3 rounded-2xl text-center max-w-sm' />
+        <input    onChange={handleChange} id='appointmentDate' placeholder='Appointment Date' type='datetime-local' required className='bg-base-300 w-11/12 p-3 rounded-2xl text-center max-w-sm' />
         <button className='btn btn-neutral rounded-2xl w-11/12 max-w-sm text-base'> Submit </button>
       </form>
     </div>
