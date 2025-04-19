a184635155707cf74d1fb903be03daaef4459321
Glennmark2002 <glennmark.culibra@gmail.com>
2025-04-19 20:12:38 +0800

    Fixed navbar Admin

--- greetly-admin/src/components/Navbar.jsx
+++ greetly-admin/src/components/Navbar.jsx
@@ -4,13 +4,12 @@ import Theme from './Theme';
 
 function Navbar() {
 
-  const { currentUser } = useStore();
   const { openSidebar } = useTempStore();
 
   return (
     <div className='navbar bg-base-100 fixed'>
       <div className='navbar-start'>
-        { currentUser && <button className='btn btn-ghost' onClick={openSidebar}> <Menu /> </button> }
+        <button className='btn btn-ghost' onClick={openSidebar}> <Menu /> </button>
       </div>
       <div className='navbar-end'>
         <Theme />
