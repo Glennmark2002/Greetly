2ec4f834914d565b066e5acb46abafd988622a71
Glennmark2002 <glennmark.culibra@gmail.com>
2025-04-20 12:10:11 +0800

    Testing Tanstack

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -1,29 +1,78 @@
-import React from 'react';
 import { useStore } from '../utils/zustand';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useState } from 'react';
 import Card from '../components/Card';
+import { useQuery } from '@tanstack/react-query';
 
 function History() {
 
   const { currentUser } = useStore();
-  const [formData, setFormData] = useState([]);
+  const fetchHistory = async () => {
+    const res = await axios.post('/api/qr/archive', { user : currentUser._id }); 
+    return res.data;
+  }
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['history', currentUser?._id],
+    queryFn: fetchHistory,
+  });  
+  
 
-  useEffect(() => { 
-    const fetchData = async () => {
-      const res = await axios.post('/api/qr/archive', { user : currentUser._id });
-      setFormData(res.data);
-    }
 
-    fetchData();
-  }, [])
+  console.log(data)
+
+  
+  // const [formData, setFormData] = useState([]);
+
+  // useEffect(() => { 
+  //   const fetchData = async () => {
+  //     const res = await axios.post('/api/qr/archive', { user : currentUser._id });
+  //     setFormData(res.data);
+  //   }
+
+  //   fetchData();
+  // }, [])
 
   return (
     <div className='h-screen flex items-center pt-20 p-4 flex-col gap-6'>
-      { formData.map(i => <Card key={i._id} data={i} />) }
+      {/* { data.map(i => <Card key={i._id} data={i} />) } */}
+    {isLoading && <div>Loading...</div>}
+    {error && <div>Error loading history.</div>}
+    {data && data.map(i => <Card key={i._id} data={i} />)}
     </div>
   );
 }
 
 export default History;
+
+
+
+
+// import React from 'react';
+// import { useStore } from '../utils/zustand';
+// import { useEffect } from 'react';
+// import axios from 'axios';
+// import { useState } from 'react';
+// import Card from '../components/Card';
+
+// function History() {
+
+//   const { currentUser } = useStore();
+//   const [formData, setFormData] = useState([]);
+
+//   useEffect(() => { 
+//     const fetchData = async () => {
+//       const res = await axios.post('/api/qr/archive', { user : currentUser._id });
+//       setFormData(res.data);
+//     }
+
+//     fetchData();
+//   }, [])
+
+//   return (
+//     <div className='h-screen flex items-center pt-20 p-4 flex-col gap-6'>
+//       { formData.map(i => <Card key={i._id} data={i} />) }
+//     </div>
+//   );
+// }
+
+// export default History;
