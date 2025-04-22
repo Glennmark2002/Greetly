aa7b22dcbea53e4dc4b230f17a8c6d1960656e66
Glennmark2002 <glennmark.culibra@gmail.com>
2025-04-22 19:26:57 +0800

    Save User to local Storage

--- client/src/components/Facebook.jsx
+++ client/src/components/Facebook.jsx
@@ -1,20 +1,33 @@
 import { getAuth, signInWithPopup, FacebookAuthProvider } from 'firebase/auth';
 import { app } from '../utils/firebase';
+import axios from 'axios';
+import { useStore } from '../utils/zustand';
+import { useNavigate } from 'react-router-dom';
 
 function Facebook() {
 
-  const auth = getAuth(app); 
-  const provider = new FacebookAuthProvider();
+  const { signInSuccess } = useStore();
+  const navigate = useNavigate();
 
   const handleClick = async () => {
     try {
-      const result = await signInWithPopup(auth, provider); 
-      console.log(result.user);
+      const auth = getAuth(app); 
+      const provider = new FacebookAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const res = await axios.post('/api/auth/google', {
+        name : result.user.displayName, 
+        email : result.user.email, 
+        photo : result.user.photoURL
+      }); 
+      
+      signInSuccess(res.data); 
+      navigate('/home');
     } catch (error) {
       console.log(error)
     }
     
   }
+
 
   return <button className='btn btn-neutral w-3/4 max-w-md' onClick={handleClick}> FishBool </button>
 }
