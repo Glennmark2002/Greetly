fcf4be9321c061baa5ea8249a1b2143f3cd97a68
Glennmark2002 <glennmark.culibra@gmail.com>
2025-04-23 21:23:49 +0800

    Testing PWA

--- scanner/vite.config.js
+++ scanner/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -11,5 +11,20 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: 
+    [
+      react(),  
+      VitePWA({
+        srcDir: 'public',
+        filename: 'site.webmanifest',
+        registerType: 'autoUpdate',
+        includeAssets: [
+          'favicon-32x32.png',
+          'favicon-16x16.png',
+          'apple-touch-icon.png'
+        ],
+        manifest: false 
+      })
+    
+    ],
 })
