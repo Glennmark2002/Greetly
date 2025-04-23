4e3066a84cb0479c94b1deea36852cea20ba4564
Glennmark2002 <glennmark.culibra@gmail.com>
2025-04-23 22:03:01 +0800

    Fixing the error

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ function Home() {
 
   return (
     <div className='h-screen pt-20 p-8 flex flex-col items-center justify-center gap-4 '>
-      { isLoading ? <p className='loading loading-dots loading-lg' /> : ( data.qr ? <img src={data.qr} className='w-64 rounded-2xl' /> :  <Link to='/request-form' className='btn btn-neutral w-11/12 max-w-sm'> Create Request </Link> ) }
+      { isLoading ? <p className='loading loading-dots loading-lg' /> : ( data?.qr ? <img src={data.qr} className='w-64 rounded-2xl' /> :  <Link to='/request-form' className='btn btn-neutral w-11/12 max-w-sm'> Create Request </Link> ) }
       { data?.status && <p className={`absolute top-24 text-lg p-2 -z-50 text-black rounded-lg ${ data.status === 'pending' ? 'bg-gray-400' : 'bg-green-400' }`}> status - {data.status} </p> }
     </div>
   );
